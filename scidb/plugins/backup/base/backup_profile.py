fbc1b7e40425e3e0da3e681ef843af175a0c5a69
oxdc <29519076+oxdc@users.noreply.github.com>
2020-01-27 13:02:07 +0800

    updated backup plugin

---
+++ scidb/plugins/backup/base/backup_profile.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import Union
+
+
+class BackupProfile:
+    def __init__(self, profile_name: Union[None, str] = None, time: Union[None, datetime] = None, **kwargs):
+        self.__profile_name__ = profile_name
+        self.__time__ = time
+        self.parse_name()
+
+    def parse_name(self):
+        if self.__profile_name__ is not None:
+            time_str = self.__profile_name__.replace('db_backup_', '').replace('.json', '')
+            self.__time__ = datetime.strptime(time_str, '%Y%m%d-%H%M%S-%f')
+        elif self.__time__ is not None:
+            self.__profile_name__ = f"db_backup_{self.__time__.strftime('%Y%m%d-%H%M%S-%f')}.json"
+        else:
+            raise AssertionError
+
+    @property
+    def name(self) -> str:
+        return f"db_backup_{self.__time__.strftime('%Y%m%d-%H%M%S-%f')}.json"
--- scidb/plugins/backup/implementations/local.py
+++ scidb/plugins/backup/implementations/local.py
@@ -2,11 +2,40 @@ from scidb.plugins.backup.base.backend import BackupBackend
 from scidb.utils.extractor import db_to_json
 from scidb.utils.iteration import iter_data
 from scidb.core import Database, Data
+from ..base.backup_profile import BackupProfile
 from typing import Tuple, List, Union
 from pathlib import Path
 from datetime import datetime
 import json
 import shutil
+
+
+class LocalBackupProfile(BackupProfile):
+    def __init__(self,
+                 profile_name: Union[None, str] = None,
+                 time: Union[None, datetime] = None,
+                 path: Union[None, str, Path] = None):
+        self.__path__ = None
+        self.set_path(path)
+        super().__init__(profile_name, time)
+
+    @property
+    def path(self):
+        return self.__path__
+
+    @property
+    def db_json(self):
+        return self.__path__ / self.name
+
+    @property
+    def obj_path(self):
+        return self.__path__ / 'objects'
+
+    def set_path(self, path: Union[None, str, Path]):
+        if path is None or isinstance(path, Path):
+            self.__path__ = path
+        else:
+            self.__path__ = Path(path)
 
 
 class LocalBackend(BackupBackend):
@@ -23,20 +52,20 @@ class LocalBackend(BackupBackend):
     def connect(self):
         self.__is_connected__ = True
 
-    def create_backup(self):
-        db_json = self.__backup_path__ / f"db_backup_{datetime.now().strftime('%Y%m%d-%H%M%S')}.json"
-        obj_path = self.__backup_path__ / 'objects'
-        obj_path.mkdir(parents=True, exist_ok=True)
-        with open(str(db_json), 'w') as fp:
+    def create_backup(self, verbose: bool = True):
+        profile = LocalBackupProfile(time=datetime.now(), path=self.__backup_path__)
+        profile.obj_path.mkdir(parents=True, exist_ok=True)
+        with open(str(profile.db_json), 'w') as fp:
             json.dump(
                 obj=db_to_json(self.__db_name__, self.__db_path__),
                 fp=fp,
                 indent=2
             )
 
-        def copy_data_objs(data: Data, root_path: Path = obj_path):
-            print(data.name, data.path)
-            dst_path = root_path / data.sha1()
+        def copy_data_objs(data: Data):
+            if verbose:
+                print(data.name, data.path)
+            dst_path = profile.obj_path / data.sha1()
             if not dst_path.exists():
                 shutil.copyfile(
                     src=str(data.path),
@@ -44,14 +73,14 @@ class LocalBackend(BackupBackend):
                 )
 
         for bucket in self.__db__.all_buckets:
-            iter_data(bucket, copy_data_objs, include_deleted=True, root_path=obj_path)
+            iter_data(bucket, copy_data_objs, include_deleted=True)
 
     def sync_backup(self):
         pass
 
-    def list_backups(self) -> List[Tuple[str, Path]]:
-        backups = [child for child in self.__backup_path__.glob('*') if child.is_dir()]
-        return [(backup.name, backup) for backup in backups]
+    def list_backups(self) -> List[LocalBackupProfile]:
+        backups = [child for child in self.__backup_path__.glob('db_backup_*.json') if child.is_file()]
+        return [LocalBackupProfile(profile_name=backup.name, path=self.__backup_path__) for backup in backups]
 
     def fetch_backup(self, time: datetime) -> Tuple[str, Path]:
         backup_path = self.__backup_path__ / f"db_backup_{time.strftime('%Y%m%d-%H%M%S')}"
